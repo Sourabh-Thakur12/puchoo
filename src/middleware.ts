3f3247b00272a723e6af39f9333a8156affcc196
Sourabh-Thakur12 <sourabhthakur0423@gmail.com>
2025-06-13 16:47:35 +0530

    added {authProvider, middleware}

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,35 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from 'next-auth/jwt'
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {
+
+  const token = await getToken({req: request})
+  const url = request.nextUrl
+
+  if(token && 
+    (
+      url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/'
+    )
+  ){
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  // return NextResponse.redirect(new URL('/home', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+  matcher: [
+    '/sign-in',
+    '/sign-up',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
 }
